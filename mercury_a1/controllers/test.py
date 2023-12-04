222cb7e070e5f56c970395e43d4d9797dffcda5d
wangWking <842749351@qq.com>
2023-12-04 13:57:39 +0800

    add mercury A1 mercury B1 model

---
+++ mercury_a1/controllers/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# @File    : test.py
+# @Author  : Wang Weijian
+# @Time    :  2023/11/30 13:53:04
+# @function: the script is used to do something
+# @version : V1
+
+import controller
+from controller import Robot
+
+print('hello world')
